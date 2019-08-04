cf7cf26218e4d794e66cc2dff501c93ecc3f7996
Pablo Piovano <ppiova@hotmail.com>
2019-08-04 02:49:50 -0300

    Add Controller and View Areas

---
+++ Firefighters.Web/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Firefighters.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Firefighters.Web.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        //Here save the Entities
+        //public DbSet<Owner> Owners { get; set; }
+
+        public DbSet<Area> Areas { get; set; }
+    }
+}
---
+++ Firefighters.Web/Data/Entities/Area.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Firefighters.Web.Data.Entities
+{
+    public class Area
+    {       
+
+        [Display(Name = "Nro. Area")]
+        [Range(0, Int16.MaxValue, ErrorMessage = "El {0} campo no debe tener más de {2} caractéres.")] 
+        [Required(ErrorMessage = "El campo {0} es Obligatorio.")]
+        public Int16 Id { get; set; }
+
+        [Display(Name = "Area")]
+        [MaxLength(20, ErrorMessage = "El {0} campo no debe tener más de {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es Obligatorio.")]
+        public string AreaName { get; set; }
+
+
+
+    }
+}
